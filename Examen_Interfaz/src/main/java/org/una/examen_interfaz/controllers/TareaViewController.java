5355b9dedcf2777b39f6ada01c1b5a869a310102
thonyva <anthony.vargas.alvarado@est.una.ac.cr>
2020-10-30 10:31:16 -0600

    Creadas las vistas para el requerimiento 7.1.

---
+++ Examen_Interfaz/src/main/java/org/una/examen_interfaz/controllers/TareaViewController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.una.examen_interfaz.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author thony
+ */
+
+public class TareaViewController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
