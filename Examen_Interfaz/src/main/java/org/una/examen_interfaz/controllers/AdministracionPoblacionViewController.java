00c63ee9b0fc7e2c3e7af7991c999621a756f65f
Erikgach <69018839+Erikgach@users.noreply.github.com>
2020-10-30 13:41:18 -0600

    Se agregan Formulario

---
+++ Examen_Interfaz/src/main/java/org/una/examen_interfaz/controllers/AdministracionPoblacionViewController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.examen_interfaz.controllers;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author erikg
+ */
+public class AdministracionPoblacionViewController implements Initializable {
+
+    @FXML
+    private JFXTextField txtId;
+    @FXML
+    private JFXButton btnBuscar;
+    @FXML
+    private JFXButton btnActualizar;
+    @FXML
+    private JFXButton btnNuevo;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void onActionButtonBuscar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionButtonActualizar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionButtonNuevo(ActionEvent event) {
+    }
+    
+}
---
+++ Examen_Interfaz/src/main/java/org/una/examen_interfaz/controllers/PoblacionViewController.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.examen_interfaz.controllers;
+
+import com.jfoenix.controls.JFXButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author erikg
+ */
+public class PoblacionViewController implements Initializable {
+
+    @FXML
+    private JFXButton btnProvincia;
+    @FXML
+    private JFXButton btnCanton;
+    @FXML
+    private JFXButton btnDistrito;
+    @FXML
+    private JFXButton btnUnidad;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void onActionButtonProvincia(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionButtonCanton(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionButtonDistrito(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionButtonUnidad(ActionEvent event) {
+    }
+    
+}
